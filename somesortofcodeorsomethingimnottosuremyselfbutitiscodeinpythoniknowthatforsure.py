80dc10596ded2043a4abccd7d34b18fe901c700a
x <110073264+XarniumX@users.noreply.github.com>
2023-09-05 10:13:17 -0700

    Update somesortofcodeorsomethingimnottosuremyselfbutitiscodeinpythoniknowthatforsure.py
    Added some goofy stuff

--- somesortofcodeorsomethingimnottosuremyselfbutitiscodeinpythoniknowthatforsure.py
+++ somesortofcodeorsomethingimnottosuremyselfbutitiscodeinpythoniknowthatforsure.py
@@ -1,7 +1,7 @@
 #This is how we learned to concatenate strings in the ITF+ class.
 a = "String 1"
 b = "String 2"
-print("a =" + a + " and  b=" + b)
+print("a = " + a + " and  b = " + b)
 
 #This syntax utilizes the concept of the f-string, which makes things a bit easier. No + sign needed!
 x = "String 3"
@@ -10,3 +10,8 @@ print(f"x = {x} and y = {y}")
 
 #We could also use an f-string like this:
 print("c = {c} and d = {d}".format(c=1, d=2))
+
+the = {'John': 'Some random dude', 'Jane': 'AI built npc', 'Jim': 'He isnt really to sure himself'}
+
+for name, t in the.items():
+        print(f"{name} is, {t}")
