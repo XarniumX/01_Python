49a403a1f82303e8006a3c782ffdb1279c1686ca
x <110073264+XarniumX@users.noreply.github.com>
2023-09-05 10:18:16 -0700

    Update somesortofcodeorsomethingimnottosuremyselfbutitiscodeinpythoniknowthatforsure.py
    Not the password trust

--- somesortofcodeorsomethingimnottosuremyselfbutitiscodeinpythoniknowthatforsure.py
+++ somesortofcodeorsomethingimnottosuremyselfbutitiscodeinpythoniknowthatforsure.py
@@ -1,3 +1,6 @@
+def test_password(x):
+    print(x)
+
 #This is how we learned to concatenate strings in the ITF+ class.
 a = "String 1"
 b = "String 2"
@@ -14,4 +17,7 @@ print("c = {c} and d = {d}".format(c=1, d=2))
 the = {'John': 'Some random dude', 'Jane': 'AI built npc', 'Jim': 'He isnt really to sure himself'}
 
 for name, t in the.items():
-        print(f"{name} is, {t}")
+    print(f"{name} is, {t}")
+
+Password = 'ThisIsThePassword'
+test_password(Password)
